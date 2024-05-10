0536052f87a277df8fcb5f439b1aee60b2b163e4
nguyenkkent <nguyen.k.kent@gmail.com>
2024-05-09 17:40:36 -0700

    Stash for pull

--- bcm2835-1.70/Motor_Driver_HAT_Code/Motor_Driver_HAT_Code/Raspberry Pi/c/examples/main.c
+++ bcm2835-1.70/Motor_Driver_HAT_Code/Motor_Driver_HAT_Code/Raspberry Pi/c/examples/main.c
@@ -19,12 +19,38 @@ void  Handler(int signo)
     exit(0);
 }
 
-
 void goStraight(){
     printf("Going forward at 100 speed\n");
     Motor_Run(FORWARD, 100);
 }
 
+void turnLeft(){
+    int pwm = 100;
+    while(gpioRead(IR_SENSOR)){
+        pwm = pwm - 10;
+        //DECREASE LEFT MOTOR
+    }
+    //assertion: car is centered
+    //motor foward 100%
+}
+
+void turnRight(){
+    int pwm = 100;
+    while(gpioRead(IR_SENSOR)){
+        pwm = pwm - 10;
+        //DECREASE RIGHT MOTOR
+    }
+    //assertion: car is centered
+    //motor foward 100%
+}
+
+
+void crab(){
+    /*
+    left front and back right motor set to
+
+    */
+}
 int main(void)
 {
     //1.System Initialization
@@ -32,13 +58,13 @@ int main(void)
         exit(0);
     
     //setup GPIO
-	// if (gpioInitialise() == PI_INIT_FAILED){
-	// 	printf("Failed to initialize\n");
-	// 	return 0;
-	// }
-    // gpioSetMode(LEFT_LINE_SENSOR, PI_INPUT);
-    // gpioSetMode(RIGHT_LINE_SENSOR, PI_INPUT);
-    // gpioSetMode(IR_SENSOR, PI_INPUT);
+	if (gpioInitialise() == PI_INIT_FAILED){
+		printf("Failed to initialize\n");
+		return 0;
+	}
+    gpioSetMode(LEFT_LINE_SENSOR, PI_INPUT);
+    gpioSetMode(RIGHT_LINE_SENSOR, PI_INPUT);
+    gpioSetMode(IR_SENSOR, PI_INPUT);
 
     //start motor
     void goStraight();
@@ -48,7 +74,7 @@ int main(void)
     signal(SIGINT, Handler);
     while(1) {
     }
-
+    
     //3.System Exit
     DEV_ModuleExit();
     return 0;
