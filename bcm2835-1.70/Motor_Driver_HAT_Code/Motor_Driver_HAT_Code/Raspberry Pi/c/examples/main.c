21b0e13be563ecda20599753f3c674c8c7a6a797
nguyenkkent <nguyen.k.kent@gmail.com>
2024-05-20 15:52:31 -0700

    Adjusted to 10 offset

--- bcm2835-1.70/Motor_Driver_HAT_Code/Motor_Driver_HAT_Code/Raspberry Pi/c/examples/main.c
+++ bcm2835-1.70/Motor_Driver_HAT_Code/Motor_Driver_HAT_Code/Raspberry Pi/c/examples/main.c
@@ -49,8 +49,8 @@ int crab(){
     }
     clock_t end_time = clock();
     double loop_duration = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
-    double loop_duration_adjusted = loop_duration*10;
-    printf("Loop duration : %f seconds\n", loop_duration_adjusted );
+    double loop_duration_adjusted = loop_duration*15;
+    printf("Loop duration : %f seconds\n", loop_duration);
     printf("Loop duration ADJUSTED : %f seconds\n", loop_duration_adjusted );
 
     sleep((unsigned int)loop_duration_adjusted);
